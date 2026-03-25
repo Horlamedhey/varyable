b031461938364ff3767e56f4cdda3e198d3d3b39
AbdulGafar Olamide Ajao /vary-able <Gafarolamide.ajao@gmail.com>
2026-03-25 15:07:42 +0400

    Add SVG favicon with ICO fallback

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,8 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/svg+xml" href={favicon} />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="any" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
